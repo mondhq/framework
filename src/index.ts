d437488c7d7f79b6233b34fb663f2d6820702ef0
OfficialCRUGG <ich@crugg.de>
2022-11-06 17:27:24 +0100

    ✨ Added embed export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as Mond } from "./Mond";
 // Builders
 export { default as MondCommand } from "./builders/MondCommand";
 export { default as MondEvent } from "./builders/MondEvent";
+export { default as MondEmbed } from "./builders/MondEmbed";
 
 // Interfaces
 export { default as MondConfig } from "./interfaces/config/MondConfig";
